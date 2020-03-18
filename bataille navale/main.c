0a2c5f22a80e66029946e9d6ad43b195e43a9101
Beusis <61867771+Beusis@users.noreply.github.com>
2020-03-18 23:24:05 +0100

    Réalisation écran d'aide et map
    Réalisation d'un écran d'aide et d'une map

--- bataille navale/main.c
+++ bataille navale/main.c
@@ -3,8 +3,26 @@
 #include <stdio.h>
 #include <math.h>
 
-int choix;
+int choix, coordonnéesY;
+char coordonnéesX;
 
+#define MAP_X 9
+#define MAP_Y 9
+
+#define MAP {\
+{0, 0, 0, 0, 2, 0, 2, 0, 0, 0},\
+{0, 0, 0, 1, 2, 0, 2, 0, 0, 0},\
+{0, 1, 0, 1, 0, 0, 2, 0, 0, 2},\
+{0, 1, 0, 1, 0, 0, 0, 0, 0, 2},\
+{0, 1, 0, 1, 0, 1, 1, 1, 0, 2},\
+{2, 0, 2, 2, 2, 0, 0, 0, 0, 2},\
+{2, 0, 0, 0, 0, 0, 0, 0, 0, 2},\
+{2, 0, 1, 1, 1, 1, 1, 0, 0, 0},\
+{2, 0, 0, 0, 0, 0, 0, 0, 1, 0},\
+{0, 0, 0, 0, 0, 0, 0, 0, 1, 0}\
+}
+
+int map[MAP_Y][MAP_X] = MAP;
 #pragma set_character_execution
 
 void accent () {
@@ -17,10 +35,10 @@ void pause () {
 
 void afficherMenu () {
     printf("          <----Bataille Navale---->\n\n\n\n\n\n");
-    printf("          1 - Jouer\n");
-    printf("          2 - Aide\n");
-    printf("          3 - HighScore\n");
-    printf("          4 - Quitter\n");
+    printf("               1 - Jouer\n");
+    printf("               2 - Aide\n");
+    printf("               3 - HighScore\n");
+    printf("               4 - Quitter\n");
     scanf("%d", &choix);
 }
 
@@ -28,18 +46,54 @@ void clear () {
     system("cls");
 }
 
+void aide () {
+    printf("               Aide\n\n");
+    printf("  Il y a 5 bateaux dans la carte\n");
+    printf("     1 porte-avions (5 cases)\n");
+    printf("       1 croiseur (4 cases)\n");
+    printf("   2 contre-torpilleurs (3 cases)\n");
+    printf("       1 torpilleur (2 cases)\n\n");
+    printf("Il faut tirer des obus dans la carte pour toucher les bateaux\n");
+}
+
+void afficherMap() {
+    printf("     A  B  C  D  E  F  G  H  I  J\n");
+
+    for (int i = 0, b = 0; i < 10, b < 10; i++, b++) {
+        printf(" %d ", b);
+        for (int a = 0; a < 10; a++) {
+            printf("[ ]");
+        }
+        printf("\n");
+    }
+    printf("\n\nEntrer la coordonnées X (A, B, etc)\n");
+    scanf("%c", coordonnéesX);
+    printf("Entrer la coordonnées Y (1, 2, etc)");
+    scanf("%d", coordonnéesY);
+}
+
 void menu () {
     do {
         switch (choix) {
             case 1:
+                clear();
+                afficherMap();
                 break;
             case 2:
+                clear();
+                aide();
+                pause();
+                choix = 0;
+                clear();
+                afficherMenu();
                 break;
             case 3:
                 break;
             case 4:
-                clear();
+                pause();
                 break;
+            default:
+                printf("non disponible");
         }
     } while (choix != 4);
 
@@ -51,8 +105,6 @@ int main() {
     afficherMenu();
 
     menu();
-
-    pause();
 
     return 0;
 }
